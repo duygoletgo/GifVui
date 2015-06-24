a0b97deac3a293099a1d3691068825f621105434
duygoletgo <khacduy2704@gmail.com>
2015-06-24 12:01:39 +0700

    all

---
+++ GifVui/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GifVui
+//
+//  Created by Duy on 6/6/15.
+//  Copyright (c) 2015 QuanChung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GifVui/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GifVui
+//
+//  Created by Duy on 6/6/15.
+//  Copyright (c) 2015 QuanChung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
